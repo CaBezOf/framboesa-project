e663d7effc7daefeff7423995ccafbd5a7dd8174
caio de oliveira bezerra <57061719+CaBezOf@users.noreply.github.com>
2024-05-12 17:51:06 -0300

    tela home inserção gif criação título

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainBackgroundComponent } from './main-background/main-background.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainBackgroundComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/main-background/main-background.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-background',
+  templateUrl: './main-background.component.html',
+  styleUrl: './main-background.component.scss'
+})
+export class MainBackgroundComponent {
+
+}
